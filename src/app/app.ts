3434d79fa0d0cc29b6d061176d320bfa34cbc3c5
Nowbut <manarsh.nowbut@accenture.com>
2025-06-01 15:07:48 +0400

    Section 2 Lesson 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../Shared/models/wishlist';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
