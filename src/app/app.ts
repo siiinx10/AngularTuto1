7af74a7c1ea996287a8776004fcd3c959d50d99c
Nowbut <manarsh.nowbut@accenture.com>
2025-06-01 14:18:13 +0400

    Added WishItems

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../Shared/models/wishlist';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  items = [
+    new WishItem('To learn Angular'),
+    new WishItem('Get Coffee'),
+    new WishItem('FInd grass')
+  ]
   protected title = 'wishlist';
 }
